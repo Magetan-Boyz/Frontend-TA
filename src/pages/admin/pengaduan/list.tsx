8a6527cfe5770dd3a738a6fa4dac97d084776163
fatihabdi <dotaawtf23@gmail.com>
2024-06-16 15:54:14 +0700

    feat: Add modal for creating balasan pengaduan in admin page

--- src/pages/admin/pengaduan/list.tsx
+++ src/pages/admin/pengaduan/list.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { useState } from 'react';
 import AuthenticatedLayout from '@/components/layout/layoutAdmin/AuthenticatedLayout';
 import Seo from '@/components/Seo';
-import { useRouter } from 'next/router';
 import PrimaryButton from '@/components/PrimaryButton';
 import CardPengaduan from '@/components/pengaduan/CardPengaduanSiswa';
+import { PiFlagBannerBold } from 'react-icons/pi';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure } from '@chakra-ui/react';
+import SecondaryButton from '@/components/SecondaryButton';
 
 const initialDataPengaduan = [
   {
@@ -17,7 +19,7 @@ const initialDataPengaduan = [
 ];
 
 export default function Pengaduan() {
-  const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [dataPengaduan, setDataPengaduan] = useState(initialDataPengaduan);
 
   const handleDeletePengaduan = (id: number) => {
@@ -32,10 +34,7 @@ export default function Pengaduan() {
           <div className="flex flex-col justify-between gap-5 p-3 lg:flex-row lg:border-b border-Gray-200">
             <h1 className="text-lg font-semibold">List Balasan Pengaduan</h1>
             <div className="flex flex-col items-center gap-2 lg:flex-row">
-              <PrimaryButton
-                btnClassName="font-semibold w-full lg:w-fit h-fit"
-                onClick={() => router.push('/siswa/profil/pengaduan/create')}
-              >
+              <PrimaryButton btnClassName="font-semibold w-full lg:w-fit h-fit" onClick={onOpen}>
                 Posting Balasan Pengaduan
               </PrimaryButton>
             </div>
@@ -54,6 +53,43 @@ export default function Pengaduan() {
             })}
           </div>
         </div>
+        <Modal isOpen={isOpen} onClose={onClose} isCentered>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>
+              <div className="p-2 rounded-md w-[36px] shadow-md border border-Gray-200 bg-Base-white">
+                <PiFlagBannerBold className="rotate-0" />
+              </div>
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <h1 className="text-lg font-semibold">Buat Balasan Pengaduan</h1>
+              <p className="text-sm font-light text-Gray-600">Tulis balasan dari Pengaduan yang masuk ke Whatsapp Admin</p>
+              <form action="" className="mt-3">
+                <label htmlFor="judul" className="text-sm text-Gray-600">
+                  Subject
+                </label>
+                <input type="text" id="judul" className="w-full p-2 mt-2 mb-2 border-2 rounded-md border-Gray-300" />
+                <label htmlFor="deskripsi" className="text-sm text-Gray-600">
+                  Tulisan Balasan Pengaduan
+                </label>
+                <textarea
+                  id="deskripsi"
+                  placeholder="cth : Tujuan diadakan acara ini adalah..."
+                  className="w-full p-2 mt-2 mb-2 border-2 rounded-md border-Gray-300"
+                />
+              </form>
+            </ModalBody>
+            <ModalFooter className="flex justify-center gap-3">
+              <SecondaryButton onClick={onClose} btnClassName="font-semibold">
+                Batal
+              </SecondaryButton>
+              <PrimaryButton onClick={onClose} btnClassName="font-semibold">
+                Posting Pengaduan
+              </PrimaryButton>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </AuthenticatedLayout>
     </div>
   );
